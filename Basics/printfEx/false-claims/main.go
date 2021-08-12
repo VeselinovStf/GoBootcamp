1428a1af7efb8e8c86a8ade6ccf3f2b2a2f72ef8
Stefan Veselinov <chofexx@gmail.com>
2021-08-12 14:10:58 +0300

    Adding Printf Exercis solutions

---
+++ Basics/printfEx/false-claims/main.go
@@ -0,0 +1,26 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: False Claims
+//
+//  Use printf to print the expected output using a variable.
+//
+// EXPECTED OUTPUT
+//  These are false claims.
+// ---------------------------------------------------------
+
+func main() {
+	tf := false
+
+	fmt.Printf("These are %t claims.\n", tf)
+}
---
+++ Basics/printfEx/print-your-fullname/main.go
@@ -0,0 +1,38 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Print Your Fullname
+//
+//  1. Get your name and lastname from the command-line
+//  2. Print them using Printf
+//
+// EXAMPLE INPUT
+//  Inanc Gumus
+//
+// EXPECTED OUTPUT
+//  Your name is Inanc and your lastname is Gumus.
+// ---------------------------------------------------------
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Please provide first name and last name as arguments")
+		return
+	}
+
+	msg := "Your name is %s and your lastname is %s.\n"
+	fmt.Printf(msg,args[0],args[1])
+}
