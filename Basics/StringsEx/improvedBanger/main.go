6a8c6ea403356cf3ac09bf6bb82987531c8d4fd7
Stefan Veselinov <chofexx@gmail.com>
2021-08-12 13:35:27 +0300

    Adding Basic String Exercises Solutions

---
+++ Basics/StringsEx/improvedBanger/main.go
@@ -0,0 +1,42 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Improved Banger
+//
+//  Change the Banger program the work with Unicode
+//  characters.
+//
+// INPUT
+//  "İNANÇ"
+//
+// EXPECTED OUTPUT
+//  İNANÇ!!!!!
+// ---------------------------------------------------------
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Please provide name parametrer!")
+		return
+	}
+
+	msg := args[0]
+	s := msg + strings.Repeat("!", utf8.RuneCountInString(msg))
+
+	fmt.Println(s)
+}
